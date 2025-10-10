25f52d2d2a0dfcbfe714183c13cc00ed89ff832e
Aina Nadal <aina.nadal0@gmail.com>
2025-10-10 12:32:15 +0200

    Added a video Side-by-Side

--- scripts/object_depth_video_estimation.py
+++ scripts/object_depth_video_estimation.py
@@ -60,7 +60,34 @@ def main():
             ret, frame = cap.read()
             if not ret:
                 break  # End of video
+            # ------------------------------
+            # Makes a video in which all objects will appear with different colour depending on their depth
+            # ------------------------------
+            # Convert BGR (OpenCV) to RGB (MiDaS expects RGB)
+            img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+            # Apply MiDaS transform and add batch dimension
+            input_batch = transform(img_rgb).to(device)  # Shape: [1, 3, H, W]
+
+
+            # Predict depth
+            depth_pred = midas(input_batch)
+
+            # Resize depth map to original frame size
+            depth_pred = torch.nn.functional.interpolate(
+                depth_pred.unsqueeze(1),             # Add channel dimension for interpolation
+                size=img_rgb.shape[:2],              # Resize to original frame height and width
+                mode="bicubic",
+                align_corners=False
+            ).squeeze().cpu().numpy()                # Remove extra dims and move to CPU
+
+            # Normalize depth for visualization
+            depth_vis = cv2.normalize(depth_pred, None, 0, 255, cv2.NORM_MINMAX)
+            depth_vis_color = cv2.applyColorMap(depth_vis.astype(np.uint8), cv2.COLORMAP_MAGMA)    
+
+            # ------------------------------
+            # Makes a video in which the detected objects appear with their estimated depths.
+            # ------------------------------
             results = model(frame)
             detections = results.xyxy[0]
             # Gets the information of detected objects
@@ -109,11 +136,13 @@ def main():
                     cv2.putText(frame, label, (text_x, text_y),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
 
-                    # Show the depth video
-                    cv2.imshow("Depth Map", frame)
+                    # Combine the two frames side by side ---
+                    combined = np.hstack((frame, depth_vis_color))
+                    cv2.imshow("Depth Detected Person (Left)| Depth Map (Right)", combined)
             else:
-                # Show the original video
-                cv2.imshow("Depth Map", frame)
+                # Combine the two frames side by side ---
+                combined = np.hstack((frame, depth_vis_color))
+                cv2.imshow("Depth Detected Person (Left)| Depth Map (Right)", combined)
 
             # Press 'q' to exit early
             if cv2.waitKey(1) & 0xFF == ord("q"):
